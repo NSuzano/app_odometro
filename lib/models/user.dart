7fdc6d3063fd26b3f288e4095fc899da016f1724
nathan.sineiro <nathan.sineiro@devnology.com.br>
2023-11-15 10:53:29 -0300

    feat: Criação do model User

---
+++ lib/models/user.dart
@@ -0,0 +1,52 @@
+class User {
+  int? id;
+  String? name;
+  String? email;
+  int? status;
+  String? role;
+  String? image;
+  String? emailVerifiedAt;
+  String? createdAt;
+  String? updatedAt;
+
+  User(
+      {this.id,
+      this.name,
+      this.email,
+      this.status,
+      this.role,
+      this.image,
+      this.emailVerifiedAt,
+      this.createdAt,
+      this.updatedAt});
+
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    status = json['status'];
+    role = json['role'];
+    image = json['image'];
+    emailVerifiedAt = json['email_verified_at'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['email'] = this.email;
+    data['status'] = this.status;
+    data['role'] = this.role;
+    data['image'] = this.image;
+    data['email_verified_at'] = this.emailVerifiedAt;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    return data;
+  }
+
+  static Future<User?> login() async {
+    User? user;
+  }
+}
