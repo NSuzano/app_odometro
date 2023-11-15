97bd7a2a8a7b5fc4b444c042c48abc9af9d03ea9
nathan.sineiro <nathan.sineiro@devnology.com.br>
2023-11-15 10:54:04 -0300

    feat: Nova constraint url

--- lib/constraint/constraint.dart
+++ lib/constraint/constraint.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kDefaultColors = Colors.indigo;
+
+const kDefautUrl = "http://localhost/odometer/public/api/";
+
+const kLogin = "$kDefautUrl/login";
