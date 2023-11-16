1b71fefb070e5a9a8e6dcd6164d95ae783599f31
nathan.sineiro <nathan.sineiro@devnology.com.br>
2023-11-15 22:27:07 -0300

    feat: Nova rota para Corrida - Mudança no ip

--- lib/constraint/constraint.dart
+++ lib/constraint/constraint.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const kDefaultColors = Colors.indigo;
 
-const kDefautUrl = "http://localhost/odometer/public/api/";
+const kDefautUrl = "http://10.0.2.2/odometer/public/api";
 
 const kLogin = "$kDefautUrl/login";
+const kRacePost = "$kDefautUrl/races";
