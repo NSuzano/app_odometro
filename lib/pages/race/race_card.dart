cafa3bd2141c86598114151f1fc61fcf79766dd6
Desenvolvedor02 <desenvolvedor02@workingplus.com.br>
2024-04-19 11:31:51 -0300

    :bug: fix: Melhorado a página de card da corrida

--- lib/pages/race/race_card.dart
+++ lib/pages/race/race_card.dart
@@ -10,79 +10,94 @@ class RaceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String brazilianDate = convertIsoDateToBrazilian(race.raceStart!.date!);
+    String brazilianDate = convertIsoDateToBrazilian(
+        race.raceStart!.date!); // Supondo que a função converta corretamente
 
     return Card(
-      color: race.raceEnd == null ? Colors.red : kDefaultColors,
       margin: const EdgeInsets.all(8.0),
       child: Row(
-        // Mudança de Column para Row para posicionar o ícone à esquerda
         children: [
-          // Coluna para o ícone
-          Container(
-            width: 10,
-            color: race.raceEnd == null ? Colors.redAccent : Colors.green,
-          ),
           Expanded(
-            // Container expandido para os outros widgets
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment
+                  .stretch, // Faz os filhos preencherem o espaço
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 10),
+                  decoration: BoxDecoration(
+                      color:
+                          race.raceEnd == null ? kDefaultColors : Colors.green,
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        race.status == "completed"
-                            ? "Valor: R\$ ${race.value} "
-                            : 'Kilômetros Inicial: ${race.raceStart!.odometer} KM',
-                        style: const TextStyle(
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            race.status == "completed"
+                                ? "Valor: R\$ ${race.value} "
+                                : 'KM Inicial: ${race.raceStart!.odometer} KM',
+                            style: const TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          race.status == "completed"
+                              ? const Icon(
+                                  Icons.check_circle,
+                                  color: Colors.white,
+                                )
+                              : const Text("")
+                        ],
                       ),
-                      race.status == "completed"
-                          ? const Icon(
-                              Icons.check_circle,
-                              color: Colors.white,
+                      const SizedBox(height: 10),
+                      const Divider(
+                        thickness: 2,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      race.status != "started"
+                          ? Text(
+                              "KM Inicial : ${race.raceStart!.odometer} - Km Final : ${race.raceEnd!.odometer}",
+                              style: const TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold,
+                              ),
                             )
-                          : const Text("")
-                    ],
-                  ),
-                  const Divider(
-                    color: Colors.white,
-                    thickness: 2,
-                  ),
-                  const SizedBox(height: 10),
-                  race.status != "started"
-                      ? Text(
-                          "KM Inicial : ${race.raceStart!.odometer} - Km Final : ${race.raceEnd!.odometer}",
-                          style: const TextStyle(
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                        )
-                      : const SizedBox(),
-                  const SizedBox(height: 8.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Data: $brazilianDate',
-                        style: const TextStyle(
-                            fontSize: 12.0, color: Colors.white),
-                      ),
-                      const SizedBox(width: 8.0),
-                      Text(
-                        'Hora: ${race.raceStart!.time}',
-                        style: const TextStyle(
-                            fontSize: 12.0, color: Colors.white),
+                          : const SizedBox(),
+                      const SizedBox(height: 8.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Data: $brazilianDate',
+                            style: const TextStyle(
+                              fontSize: 12.0,
+                            ),
+                          ),
+                          const SizedBox(width: 8.0),
+                          Text(
+                            'Hora: ${race.raceStart!.time}',
+                            style: const TextStyle(
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
