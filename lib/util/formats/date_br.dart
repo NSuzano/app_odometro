7e694a8817c12fab9027133e48b0dcafe2c97016
Desenvolvedor02 <desenvolvedor02@workingplus.com.br>
2024-04-19 11:20:29 -0300

    :sparkles: feat: Adicionado nova função de formatação de data

--- lib/util/formats/date_br.dart
+++ lib/util/formats/date_br.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 String convertIsoDateToBrazilian(String isoDate) {
   // Divide a data em componentes de ano, mês e dia
   List<String> parts = isoDate.split('-');
@@ -6,4 +8,21 @@ String convertIsoDateToBrazilian(String isoDate) {
   }
   // Reorganiza os componentes para o formato DD/MM/YYYY
   return '${parts[2]}/${parts[1]}/${parts[0]}';
+}
+
+
+String formatDate(String dateStr) {
+  // A entrada é assumida como '2024-03-20 23:59:00'
+  List<String> parts = dateStr.split(' '); // Divide em data e hora
+  List<String> dateParts = parts[0].split('-'); // Divide a data em [ano, mês, dia]
+
+  // Reordena para DD/MM/YYYY e reanexa a hora
+  return '${dateParts[2]}/${dateParts[1]}/${dateParts[0]} ${parts[1]}';
+}
+
+String formatDateTimeStamp(DateTime timestamp) {
+  // Define o formato desejado
+  DateFormat formatter = DateFormat('dd/MM/yyyy HH:mm:ss');
+  // Converte o timestamp para string usando o formato definido
+  return formatter.format(timestamp);
 }
