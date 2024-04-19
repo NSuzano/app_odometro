d7dd6e3eb2a78015a75df34a5725cf13e05f7976
Desenvolvedor02 <desenvolvedor02@workingplus.com.br>
2024-04-19 15:18:22 -0300

    :sparkles: feat: Adicionado um Widget de Loading

---
+++ lib/widgets/loading_dialog.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class LoadingDialog extends StatelessWidget {
+  const LoadingDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 800),
+      curve: Curves.fastLinearToSlowEaseIn,
+      child: Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        child: Stack(
+          children: [
+            Center(
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 16,
+                        offset: Offset(0, 10),
+                      )
+                    ]),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    SizedBox(
+                        height: 50,
+                        width: 50,
+                        child: CircularProgressIndicator(
+                          strokeWidth: 5,
+                        )),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text('Carregando')
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
