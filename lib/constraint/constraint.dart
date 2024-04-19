c2eb34695a5c640f057c87790e7bf58cc8b9c39f
Desenvolvedor02 <desenvolvedor02@workingplus.com.br>
2024-04-19 09:57:38 -0300

    :sparkles: Nova Rota de listagem de usuários

--- lib/constraint/constraint.dart
+++ lib/constraint/constraint.dart
@@ -6,6 +6,7 @@ const kDefaultColorWhite = Colors.white;
 const kDefautUrl = "http://192.168.0.175:8081/odometer/public/api";
 
 const kLogin = "$kDefautUrl/login";
+const kUserGet = "$kDefautUrl/users";
 const kRacePost = "$kDefautUrl/races";
 const kRaceGet = "$kDefautUrl/reports/races";
 const kExpansesGet = "$kDefautUrl/transactions";
