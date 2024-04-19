84af0585a01a65b27fef14310b0b78074e9cfe80
Desenvolvedor02 <desenvolvedor02@workingplus.com.br>
2024-04-19 11:27:12 -0300

    :bug: fix: Novas melhorias. - Melhorado a captação do token com o provider. - Nova função que recebe todos os usuários

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,15 +2,18 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:app_odometro/constraint/constraint.dart';
+import 'package:app_odometro/util/providers/user_provider.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:provider/provider.dart';
 
 class User {
   int? id;
   String? name;
   String? email;
   int? status;
-  static late String token;
+  String? token;
   String? role;
   String? image;
   String? emailVerifiedAt;
@@ -33,6 +36,7 @@ class User {
     name = json['name'];
     email = json['email'];
     status = json['status'];
+    token = json['token'];
     role = json['role'];
     image = json['image'];
     emailVerifiedAt = json['email_verified_at'];
@@ -48,35 +52,45 @@ class User {
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['email'] = this.email;
-    data['status'] = this.status;
-    data['role'] = this.role;
-    data['image'] = this.image;
-    data['email_verified_at'] = this.emailVerifiedAt;
-    data['created_at'] = this.createdAt;
-    data['updated_at'] = this.updatedAt;
+    data['id'] = id;
+    data['name'] = name;
+    data['email'] = email;
+    data['status'] = status;
+    data['token'] = token;
+    data['role'] = role;
+    data['image'] = image;
+    data['email_verified_at'] = emailVerifiedAt;
+    data['created_at'] = createdAt;
+    data['updated_at'] = updatedAt;
     return data;
   }
 
-  static Future<User?> login(String email, String password) async {
+  static Future<User?> login(
+      String email, String password, BuildContext context) async {
     User? user;
     Map jsonPost = {"email": email, "password": password};
+    final provider = Provider.of<UserProvider>(context, listen: false);
+
     try {
       var response = await http.post(Uri.parse(kLogin),
           headers: {"Accept": "application/json"}, body: jsonPost);
       Map jsonResponse = jsonDecode(response.body);
 
-      print(jsonResponse);
+      print("Json Response: $jsonResponse");
 
       if (response.statusCode == 200) {
         Map<String, dynamic> userInfo =
             JwtDecoder.decode(jsonResponse['token']);
 
-        token = jsonResponse['token'];
+        print("USER INFO: $userInfo");
+
+        userInfo.addAll({"token": jsonResponse['token']});
+        print("USER INFO TOKEN: $userInfo");
 
         user = User.fromJson(userInfo);
+
+        print("Token: ${user.token}");
+        provider.saveUserToPrefs(user);
 
         return user;
       } else {
@@ -85,6 +99,33 @@ class User {
       }
     } catch (e) {
       log(e.toString());
+      return Future.error(e);
+    }
+  }
+
+  static Future<List<User>> getAllUsersFromApi(User user) async {
+    try {
+      var response = await http.get(
+        Uri.parse(kUserGet),
+        headers: {"Accept": "application/json", "Authorization": user.token!},
+      );
+      Map jsonResponse = jsonDecode(response.body);
+
+      print("Json Response: $jsonResponse");
+
+      if (response.statusCode == 200) {
+        List<dynamic> usersData = jsonResponse['users'];
+        print("Users DATA: $usersData");
+
+        // Convert the 'raceData' list into a list of Race objects
+        List<User> users =
+            usersData.map((data) => User.fromJson(data)).toList();
+
+        return users;
+      } else {
+        throw jsonResponse['message'];
+      }
+    } catch (e) {
       return Future.error(e);
     }
   }
